0b78b8377747733d163232828dd15701a5fa6b31
RobertoAsgon <robertoasgon@gmail.com>
2020-08-25 16:36:29 -0300

    fix cc

--- models/connection.js
+++ models/connection.js
@@ -6,13 +6,13 @@ const mysqlx = require('@mysql/xdevapi');
 const connection = () => {
   return mysqlx.getSession({
     user: process.env.MYSQL_USER,
-    password: process.env.MYSQL_PASSWORD, 
-    host: process.env.HOSTNAME, 
+    password: process.env.MYSQL_PASSWORD,
+    host: process.env.HOSTNAME,
     port: 33060,
     schema: 'cookmaster',
   })
   .then(async (session) => {
-    let schema;
+    const schema;
     schema = await session.getSchema('cookmaster');
     return schema;
   })
