c684e26f4eae396f122f48bee66d8dac1952b36b
Hfreitas <hfreitas86@gmail.com>
2020-08-25 01:05:22 -0300

    criada conexão com o banco de dados

--- models/connection.js
+++ models/connection.js
@@ -3,13 +3,22 @@ require('dotenv').config();
 
 let connect;
 
-module.exports = () => {
+const config = {
+  user: process.env.MYSQL_USER,
+  password: process.env.MYSQL_PASSWORD,
+  host: process.env.HOSTNAME,
+  port: 33060,
+  socketPath: '/var/run/mysqld/mysqld.sock',
+};
+
+module.exports = async () => {
   if (connect) return Promise.resolve(connect);
   try {
-const session = await mysqlx.getSession({
-
-})
+    const session = await mysqlx.getSession(config);
+    connect = await session.getSchema('cookmaster');
+    return connect;
   } catch (error) {
-
+    console.error(error);
+    process.exit(1);
   }
-}
+};
