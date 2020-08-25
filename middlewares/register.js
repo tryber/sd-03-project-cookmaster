7a77a607c779306cb91d2cfe04af6171608e59cc
Gustaff <gustavohmfigueiredo@gmail.com>
2020-08-25 15:14:36 -0300

    cc

--- middlewares/register.js
+++ middlewares/register.js
@@ -1,17 +1,23 @@
 const { register } = require('../controllers/userController.js');
 
+const EMAIL_PATTERN = /^[A-z0-9]@[A-z0-9].com$/
+
 function verifyRegister(req, res, next) {
   try {
     const { email, password, confirmPassword, firstName, lastName } = req.body;
 
     if (!email || !password || !firstName || !lastName) {
-      return res.status(400).send('Faltou informação ou informação errada');
+      return res.status(400).send('Faltou informação');
     } else if (password !== confirmPassword) {
       return res.status(400).send('senhas incompatíveis');
+    } else if (email.match(EMAIL_PATTERN)) {
+      return res.status(400).send('email inválido');
+    } else if (senha) {
+
     }
     register(email, password, firstName, lastName);
   } catch (err) {
-    console.error('on register', err);
+    // console.error('on register', err);
     res.status(500).send('Something went wrong during register');
   }
   return next();
--- models/connection.js
+++ models/connection.js
@@ -5,26 +5,25 @@ let schema;
 async function connection() {
   if (schema) return Promise.resolve(schema);
   try {
-    return schema = mysqlx
+    schema = mysqlx
     .getSession({
       host: process.env.HOSTNAME,
       user: process.env.MYSQL_USER,
       password: process.env.MYSQL_PASSWORD,
       port: 33060,
     });
+    return schema;
   } catch (err) {
-    console.error(err);
     process.exit(1);
   }
 }
 
 async function connectionDB(db) {
-  if (schema) Promise.resolve(schema);
+  if (schema) return Promise.resolve(schema);
   try {
     const conn = await connection();
     return conn.getSchema(db);
-  } catch(err) {
-    console.error(err);
+  } catch (err) {
     process.exit(1);
   }
 }
