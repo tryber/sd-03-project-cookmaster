6aa92847658b3da04123bf60f05f09209515b56f
luldelrey <rpgstrikered@gmail.com>
2020-08-25 17:56:34 -0300

    connection

---
+++ models/connection.js
@@ -0,0 +1,25 @@
+// src: https://github.com/tryber/sd-03-live-lectures/blob/catch-up/express-mvc/models/connect.js
+
+const mysqlx = require('@mysql/xdevapi');
+require('dotenv').config();
+
+let connection;
+
+module.exports = () => connection
+  ? Promise.resolve(connection)
+  : mysqlx
+    .getSession({
+      host: process.env.HOSTNAME,
+      user: process.env.MYSQL_USER,
+      password: process.env.MYSQL_PASSWORD,
+      port: '33060',
+      socketPath: '/var/run/mysqld/mysqld.sock',
+    })
+  .then(async (session) => {
+    connection = await session.getSchema('cookmaster');
+    return connection;
+  })
+  .catch((err) => {
+    console.error(err);
+    process.exit(1);
+  });
