ddc1420f9bdd4059c18d4952be91b3094651efb6
Ishidor <kevinmendessilva@hotmail.com>
2020-08-26 15:16:56 -0300

    Projeto 02 pra 03

--- index.js
+++ index.js
@@ -1,13 +1,23 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
+const { connection } = require('./database');
 
 const middlewares = require('./middlewares');
 const controllers = require('./controllers');
+const userModel = require('./models/userModel');
 
 const app = express();
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieParser());
+
+connection.connect((err) => {
+  if (err) {
+    return console.error('erro: ' + err.message);
+  }
+
+  console.log('Connected to the MySQL server.');
+});
 
 app.set('view engine', 'ejs');
 app.set('views', './views');
@@ -22,6 +32,7 @@ app.get('/admin', middlewares.auth(), (req, res) => {
 
 app.get('/login', controllers.userController.loginForm);
 app.get('/logout', controllers.userController.logout);
+app.get('/teste/:id', controllers.userController.idController);
 app.post('/login', controllers.userController.login);
 
 app.listen(3000, () => console.log('Listening on 3000'));
