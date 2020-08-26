a35d03bc14aba70d45800b1187786a5f84db2450
rodrigo-militao <rodrigo_militao@hotmail.com.br>
2020-08-26 16:58:53 -0300

    code climate

--- controllers/homeController.js
+++ controllers/homeController.js
@@ -2,11 +2,11 @@ const recipesModel = require('../models/recipesModel');
 
 const showRecipes = async (req, res) => {
   try {
-      const recipes = await recipesModel.findAllRecipes();
-      if (!req.user) return res.render('home', { recipes, user: null });
-      return res.render('home', { recipes, user: req.user });
+    const recipes = await recipesModel.findAllRecipes();
+    if (!req.user) return res.render('home', { recipes, user: null });
+    return res.render('home', { recipes, user: req.user });
   } catch (error) {
-      return error;
+    return error;
   }
 };
 
