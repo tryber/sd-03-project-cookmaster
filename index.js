ca987146438a142195f7b9832d66122981ec6d4a
Ishidor <kevinmendessilva@hotmail.com>
2020-08-26 15:24:49 -0300

    CC fix

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
-const { connection } = require('./database');
+// const { connection } = require('./database');
 
 const middlewares = require('./middlewares');
 const controllers = require('./controllers');
