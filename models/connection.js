e4447f6c77b552cc1eed89248cdbc51912f2c4e2
rdgbrezolin <brezolinr@gmail.com>
2020-08-26 17:48:36 -0300

    dotenv

---
+++ models/connection.js
@@ -0,0 +1,25 @@
+const mysqlx = require('@mysql/xdevapi');
+require('dotenv/config');
+
+let schema;
+const config = {
+  host: process.env.HOSTNAME,
+  user: process.env.MYSQL_USER,
+  password: process.env.MYSQL_PASSWORD,
+  port: 33060,
+};
+
+async function connectionDB(db) {
+  try {
+    if (schema) return Promise.resolve(schema);
+    const conn = await mysqlx.getSession(config);
+    schema = await conn.getSchema(db);
+    return schema;
+  } catch (err) {
+    return process.exit(1);
+  }
+}
+
+module.exports = {
+  connectionDB,
+};
