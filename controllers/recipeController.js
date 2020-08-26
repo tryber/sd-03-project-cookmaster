4b9c07b0a04515b463dfdba946e9f0107a713672
Hfreitas <hfreitas86@gmail.com>
2020-08-26 14:01:53 -0300

    acrescentando comentários para referenciar auxilio na construção de código

--- controllers/recipeController.js
+++ controllers/recipeController.js
@@ -2,6 +2,8 @@ const recipesModel = require('../models/recipesModel');
 
 const listRecipes = async (req, res) => {
   try {
+    /* baseado em orientação do instrutor Roz
+    durante plantão dia 26/08 */
     const recipes = await recipesModel.findAllRecipes();
     if (req.user) return res.render('home', { recipes, user: req.user });
     return res.render('home', { recipes, user: null });
