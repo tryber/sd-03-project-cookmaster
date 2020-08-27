c8421dda3501905a005cac54dd8e496f2143a9f6
brenomesquita <brenomesquitaribeiro@gmail.com>
2020-08-26 22:35:36 -0300

    exercise 2

--- controllers/userController.js
+++ controllers/userController.js
@@ -48,57 +48,40 @@ const signupForm = (req, res) => {
   });
 };
 
-const emailRegex = RegExp(
-  /^[a-zA-Z0-9.!#$%&’*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/
-);
+const emailRegex = RegExp(/^[a-zA-Z0-9.!#$%&’*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/);
 
-const validString = (value) => !/^[a-zA-Z]+$/.test(value)
+const validString = (value) => !/^[a-zA-Z]+$/.test(value);
+
+const renderReturn = (message, res) => {
+  return res.render('admin/signup', {
+    message,
+    redirect: null,
+  })
+}
 
 const signup = async (req, res, next) => {
   const { email, password, confirmPassword, name, lastName } = req.body;
-  if (!email || !password || !confirmPassword || !name || !lastName)
-  return res.render('admin/signup', {
-    message: 'Preencha todos os dados',
-    redirect: null,
-  });
 
-  if (!emailRegex.test(email))
-  return res.render('admin/signup', {
-    message: 'O email deve ter o formato email@mail.com',
-    redirect: null,
-  });
-
-  if (password.length < 6)
-  return res.render('admin/signup', {
-    message: 'A senha deve ter pelo menos 6 caracteres',
-    redirect: null,
-  });
-
-  if (password !== confirmPassword)
-  return res.render('admin/signup', {
-    message: 'As senhas tem que ser iguais',
-    redirect: null,
-  });
-
-  if (name.length < 3 || validString(name))
-  return res.render('admin/signup', {
-    message: 'O primeiro nome deve ter, no mínimo, 3 caracteres, sendo eles apenas letras',
-    redirect: null,
-  });
-
-  if (lastName.length < 3 || validString(lastName))
-  return res.render('admin/signup', {
-    message: 'O segundo nome deve ter, no mínimo, 3 caracteres, sendo eles apenas letras',
-    redirect: null,
-  });
-
-  await userModel.setUser(req.body);
-  const user = await userModel.findByEmail(email);
-  if (user)
-  return res.render('admin/signup', {
-    message: 'Cadastro efetuado com sucesso!',
-    redirect: null,
-  });
+  if (Object.values(req.body).map((el) => !el).includes(true)){
+    return renderReturn('Preencha todos os dados', res)
+  } else if(!emailRegex.test(email)) {
+    return renderReturn('O email deve ter o formato email@mail.com', res)
+  } else if(password.length < 6) {
+    return renderReturn('A senha deve ter pelo menos 6 caracteres', res)
+  } else if(password !== confirmPassword) {
+    return renderReturn('As senhas tem que ser iguais', res)
+  } else if(name.length < 3 || validString(name)) {
+    return renderReturn(
+      'O primeiro nome deve ter, no mínimo, 3 caracteres, sendo eles apenas letras', res
+    )
+  } else {
+    await userModel.setUser(req.body);
+    const user = await userModel.findByEmail(email);
+    if (user)
+    return renderReturn(
+      'Cadastro efetuado com sucesso!', res
+    );
+  }
 };
 
 const logout = (req, res) => {
