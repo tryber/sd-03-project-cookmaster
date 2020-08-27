ca47cb8c576b1730b5e149f722a6753c39b1792b
luldelrey <rpgstrikered@gmail.com>
2020-08-27 18:23:20 -0300

    cc

--- models/userModel.js
+++ models/userModel.js
@@ -1,13 +1,5 @@
 const connect = require('./connection');
 
-const TEMP_USER = {
-  id: 'd2a667c4-432d-4dd5-8ab1-b51e88ddb5fe',
-  email: 'taylor.doe@company.com',
-  password: 'password',
-  name: 'Taylor',
-  lastName: 'Doe',
-};
-
 const findByEmail = async (email) => {
   try {
     const user = await connect()
@@ -20,17 +12,17 @@ const findByEmail = async (email) => {
     )
     .then((results) => results.fetchAll()[0])
     .then(([id, email, password, first_name, last_name]) => ({
-      id, email, password, first_name, last_name
+      id, email, password, first_name, last_name,
     }));
     return user;
   } catch (err) {
     console.error(err);
-    process.exit(1)
+    process.exit(1);
   }
 };
 
 const findById = async (id) => {
-  return TEMP_USER;
+  return id;
 };
 
 module.exports = {
