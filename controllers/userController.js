bb8aba5452c5c5d811a492af69dbe9f088bc302c
fabemiliano <fab.emiliano@gmail.com>
2020-08-27 20:44:12 -0300

    first commit

--- controllers/userController.js
+++ controllers/userController.js
@@ -8,31 +8,35 @@ const loginForm = (req, res) => {
 
   if (SESSIONS[token]) return res.redirect('/');
 
-  return res.render('admin/login', {
+  return res.render('login', {
     message: null,
     redirect: req.query.redirect,
   });
 };
 
-const login = async (req, res, next) => {
-  const { email, password, redirect } = req.body;
+const registerForm = (req, res) => {
+  res.render('register', { message: null });
+};
 
-  if (!email || !password)
-    return res.render('admin/login', {
+const login = async (req, res) => {
+  const { email, password, redirect } = req.body;
+  if (!email || !password) {
+    return res.render('login', {
       message: 'Preencha o email e a senha',
       redirect: null,
     });
+  }
 
   const user = await userModel.findByEmail(email);
-  if (!user || user.password !== password)
-    return res.render('admin/login', {
+  if (!user || user.password !== password) {
+    return res.render('login', {
       message: 'Email ou senha incorretos',
       redirect: null,
     });
+  }
 
   const token = uuid();
   SESSIONS[token] = user.id;
-
   res.cookie('token', token, { httpOnly: true, sameSite: true });
   res.redirect(redirect || '/admin');
 };
@@ -43,8 +47,97 @@ const logout = (req, res) => {
   res.render('admin/logout');
 };
 
+const renderRecipes = async (_req, res) => {
+  const recipes = await userModel.showAll();
+  res.render('home', { recipes, user: null });
+};
+
+const renderAdmin = async (req, res) => {
+  const recipes = await userModel.showAll();
+  return res.render('home', { recipes, user: req.user });
+};
+
+const saveUser = (req, res) => {
+  const { message } = userModel.saveUserInDB(req.body);
+  res.render('register', { message });
+};
+
+const showRecipeInfo = async (req, res) => {
+  const { id } = req.params;
+  const recipeInfo = await userModel.getRecipeInfo(id);
+  res.render('recipeInfo', { recipeInfo, user: req.user });
+};
+
+const searchRecipe = async (req, res) => {
+  const recipes = await userModel.searchByName(req.query.recipeName);
+  res.render('search', { recipes });
+};
+
+const addRecipe = (_req, res) => {
+  res.render('newRecipe');
+};
+
+const removeItem = (_req, res) => {
+  res.render('newRecipe');
+};
+
+const saveNewRecipe = (req, res) => {
+  userModel.saveNewRecipeInDB(req.user[0], { ...req.body });
+  res.redirect('/');
+};
+
+const findRecipeToEdit = async (req, res) => {
+  const recipeInfo = await userModel.getRecipeInfo(req.params.id);
+  const ingredient = recipeInfo.ingredients.split(',');
+  if (req.user[0] !== recipeInfo.userId) return res.redirect('/');
+  return res.render('editRecipe', { recipeInfo, ingredient });
+};
+
+const editIngredients = async (req, res) => {
+  const recipeInfo = await userModel.getRecipeInfo(req.params.id);
+  const ingredient = [];
+  return res.render('editRecipe', { recipeInfo, ingredient });
+};
+
+const updateRecipe = async (req, res) => {
+  await userModel.upDateRecipeInDB(req.params.id, req.user[0], { ...req.body });
+  res.redirect('/');
+};
+
+const showPassConfirm = (req, res) => {
+  const { id } = req.params;
+  res.render('confirmDelete', { id, message: null });
+};
+
+const deleteRecipe = async (req, res) => {
+  const { id } = req.params;
+  const { message } = await userModel.checkPassword(req.user[0], req.body, id);
+  if (message === 'ok') { res.redirect('/'); }
+  if (message === 'Senha Incorreta.') { res.render('confirmDelete', { id, message }); }
+};
+
+const showMyRecipes = async (req, res) => {
+  const recipes = await userModel.showMyRecipesFromDB(req.user[0]);
+  res.render('myRecipes', { recipes, user: req.user });
+};
+
 module.exports = {
   login,
   loginForm,
   logout,
+  renderAdmin,
+  renderRecipes,
+  registerForm,
+  saveUser,
+  showRecipeInfo,
+  searchRecipe,
+  addRecipe,
+  saveNewRecipe,
+  findRecipeToEdit,
+  updateRecipe,
+  showPassConfirm,
+  deleteRecipe,
+  removeItem,
+  showMyRecipes,
+  editIngredients,
 };
--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
+const getRecipes = require('./controllers');
+const userModel = require('./models/userModel');
 
 const middlewares = require('./middlewares');
 const controllers = require('./controllers');
@@ -8,18 +10,25 @@ const controllers = require('./controllers');
 const app = express();
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieParser());
-
+app.use(express.static('./public'));
 app.set('view engine', 'ejs');
-app.set('views', './views');
-
-app.get('/', (_req, res) => {
-  return res.render('home');
-});
-
-app.get('/admin', middlewares.auth(), (req, res) => {
-  return res.render('admin/home', { user: req.user });
-});
-
+app.set('views', './views/admin');
+app.get('/', getRecipes.userController.renderRecipes);
+app.get('/register', controllers.userController.registerForm);
+app.post('/register', controllers.userController.saveUser);
+app.get('/recipe/:id', middlewares.auth(false), controllers.userController.showRecipeInfo);
+app.get('/admin', middlewares.auth(), controllers.userController.renderAdmin);
+app.get('/recipes/search', controllers.userController.searchRecipe);
+app.post('/recipes', middlewares.auth(), controllers.userController.saveNewRecipe);
+app.get('/recipes', getRecipes.userController.renderRecipes);
+app.get('/recipes/new', middlewares.auth(), controllers.userController.addRecipe);
+app.post('/recipes/new', middlewares.auth(), controllers.userController.removeItem);
+app.get('/recipes/:id/edit', middlewares.auth(), controllers.userController.findRecipeToEdit);
+app.post('/recipes/:id/edit', middlewares.auth(), controllers.userController.editIngredients);
+app.post('/recipes/:id', middlewares.auth(), controllers.userController.updateRecipe);
+app.get('/recipes/:id/delete', middlewares.auth(), controllers.userController.showPassConfirm);
+app.post('/recipes/:id/delete', middlewares.auth(), controllers.userController.deleteRecipe);
+app.get('/me/recipes', middlewares.auth(), controllers.userController.showMyRecipes);
 app.get('/login', controllers.userController.loginForm);
 app.get('/logout', controllers.userController.logout);
 app.post('/login', controllers.userController.login);
