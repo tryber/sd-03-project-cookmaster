57b2d06c0ccca3b0338a67c0066ef37d1836b58b
brenomesquita <brenomesquitaribeiro@gmail.com>
2020-08-27 05:51:47 -0300

    exercise 1 code-climete

--- controllers/cookController.js
+++ controllers/cookController.js
@@ -5,15 +5,11 @@ const listCook = async (_req, res) => {
   return res.render('home', { recipes });
 };
 
-const searchRecipe = async(_req, res) =>  res.render('searchRecipes');
+const searchRecipe = async (_req, res) => res.render('searchRecipes');
 
-const newRecipe = async (_req, res) => {
-  return res.render('admin/newRecipe');
-};
+const newRecipe = async (_req, res) => res.render('admin/newRecipe');
 
-const setNewRecipe = async (_req, res) => {
-  return res.send('ok');
-};
+const setNewRecipe = async (_req, res) => res.send('ok');
 
 const cooks = async (req, res) => {
   const { id } = req.params;
