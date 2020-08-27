50a6511834d01698041f963cbd77cb9b1e39a389
luldelrey <rpgstrikered@gmail.com>
2020-08-27 18:27:51 -0300

    cc

--- models/userModel.js
+++ models/userModel.js
@@ -1,24 +1,25 @@
 const connect = require('./connection');
 
-const findByEmail = async (email) => {
+const findByEmail = async (uEmail) => {
   try {
     const user = await connect()
       .then((db) => db
       .getTable('users')
       .select()
       .where('email = :email')
-      .bind('email', email)
+      .bind('email', uEmail)
       .execute(),
     )
     .then((results) => results.fetchAll()[0])
-    .then(([id, email, password, first_name, last_name]) => ({
-      id, email, password, first_name, last_name,
+    .then(([id, email, password, firstName, lastName]) => ({
+      id, email, password, firstName, lastName,
     }));
     return user;
   } catch (err) {
     console.error(err);
     process.exit(1);
   }
+  return 1;
 };
 
 const findById = async (id) => {
