3e332ee5f0a213f98164b03c99c6b45628187209
Marco Barbosa <marco.meireles.b@gmail.com>
2020-08-27 19:08:33 -0300

    codeclimate

--- controllers/userController.js
+++ controllers/userController.js
@@ -48,6 +48,31 @@ const logout = (req, res) => {
   res.render('admin/logout');
 };
 
+const validateFields = (form) => {
+  const errors = [];
+  // Validações
+  // Ref. Regex email em https://stackoverflow.com/questions/46155/how-to-validate-an-email-address-in-javascript
+  const regexEmail = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/g;
+  //Ref. Regex name em https://stackoverflow.com/questions/3532053/regular-expression-for-only-characters-a-z-a-z
+  const regexName = /^[a-z]*$/i;
+  if (!regexEmail.test(form.email)) {
+    errors.push('O email deve ter o formato email@mail.com');
+  }
+  if (!form.password || form.password.length < 6) {
+    errors.push('A senha deve ter pelo menos 6 caracteres');
+  }
+  if (!form.password || form.password !== form.passwordRetype) {
+    errors.push('As senhas tem que ser iguais');
+  }
+  if (!form.name || form.name.length < 3 || !regexName.test(form.name)) {
+    errors.push('O primeiro nome deve ter, no mínimo, 3 caracteres, sendo eles apenas letras');
+  }
+  if (!form.surname || form.surname.length < 3 || !regexName.test(form.surname)) {
+    errors.push('O segundo nome deve ter, no mínimo, 3 caracteres, sendo eles apenas letras');
+  }
+  return errors;
+};
+
 const signinForm = (req, res) => {
   const { token = '' } = req.cookies || {};
 
@@ -61,35 +86,16 @@ const signinForm = (req, res) => {
 };
 
 const signin = async (req, res, next) => {
-  const { email, password, passwordRetype, name, surname, redirect } = req.body;
+  // const { email, password, passwordRetype, name, surname, redirect } = req.body;
   // Validações
-  const errors = [];
-  // Ref. Regex email em https://stackoverflow.com/questions/46155/how-to-validate-an-email-address-in-javascript
-  const regexEmail = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-  //Ref. Regex name em https://stackoverflow.com/questions/3532053/regular-expression-for-only-characters-a-z-a-z
-  const regexName = /^[a-zA-Z]*$/;
-  if (!regexEmail.test(email)) {
-    errors.push('O email deve ter o formato email@mail.com');
-  }
-  if (!password || password.length < 6) {
-    errors.push('A senha deve ter pelo menos 6 caracteres');
-  }
-  if (!password || password !== passwordRetype) {
-    errors.push('As senhas tem que ser iguais');
-  }
-  if (!name || name.length < 3 || !regexName.test(name)) {
-    errors.push('O primeiro nome deve ter, no mínimo, 3 caracteres, sendo eles apenas letras');
-  }
-  if (!surname || surname.length < 3 || !regexName.test(surname)) {
-    errors.push('O segundo nome deve ter, no mínimo, 3 caracteres, sendo eles apenas letras');
-  }
+  const errors = validateFields(req.body);
 
   if (errors.length > 0) {
     return res.render('signin', {
       message: 'Erros foram encontrados:',
       redirect: null,
       error: errors,
-    });  
+    });
   }
 
   return res.render('signin', {
@@ -97,30 +103,6 @@ const signin = async (req, res, next) => {
     redirect: null,
     error: errors,
   });
-  // if (!email || !password)
-  //   return res.render('signin', {
-  //     message: 'Preencha o email e a senha',
-  //     redirect: null,
-  //   });
-
-  // const user = await userModel.findByEmail(email);
-  // if (typeof (user) === 'undefined')
-  //   return res.render('signup', {
-  //     message: 'Usuário não cadastrado',
-  //     redirect: null,
-  //   });
-
-  // if (!user || user.password !== password)
-  //   return res.render('signup', {
-  //     message: 'Email ou senha incorretos',
-  //     redirect: null,
-  //   });
-
-  // const token = uuid();
-  // SESSIONS[token] = user.id;
-
-  // res.cookie('token', token, { httpOnly: true, sameSite: true });
-  // res.redirect(redirect || '/');
 };
 
 module.exports = {
