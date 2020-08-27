314a1f2776219c07852a88c042ec1723fd8c9961
brenomesquita <brenomesquitaribeiro@gmail.com>
2020-08-27 05:05:33 -0300

    exercise 1 code-climete

--- models/userModel.js
+++ models/userModel.js
@@ -7,19 +7,22 @@ const setUser = async (userValues) =>
       .values(userValues.email, userValues.password, userValues.name, userValues.lastName)
       .execute());
 
-const findByValue = async (email, param) =>
+const findByValue = async (value, param) =>
   connect()
     .then((db) =>
       db
         .getTable('users').select(['id', 'email', 'password', 'first_name', 'last_name'])
         .where(`${param} = :email`)
-        .bind('email', email)
+        .bind('email', value)
         .execute(),
       )
       .then((results) => results.fetchAll()[0])
       .then(([id, email, password, firstName, lastName] = []) => (
-        { id, email, password, firstName, lastName }
-      ));
+        lastName
+          ?
+          { id, email, password, firstName, lastName }
+          :
+        null));
 
 module.exports = {
   findByValue,
