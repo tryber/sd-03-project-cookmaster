a4817b675621df3b031efb5758bab0b8c69b137b
brenomesquita <brenomesquitaribeiro@gmail.com>
2020-08-27 18:34:14 -0300

    exercise 1 code-climete

--- routeRecipes.js
+++ routeRecipes.js
@@ -8,7 +8,7 @@ const router = express.Router();
 
 router.post('/', middlewares.auth(), controllers.cookController.setNewRecipe);
 router.get('/new', middlewares.auth(), controllers.cookController.newRecipe);
-router.get('/shearch', middlewares.auth(false), controllers.cookController.searchRecipe);
+router.get('/search', middlewares.auth(false), controllers.cookController.searchRecipe);
 router.get('/:id', middlewares.auth(false), controllers.cookController.cooks);
 
 module.exports = router;
