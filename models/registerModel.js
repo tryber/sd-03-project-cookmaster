53ff3a41517f52d50327a57ca183004f4d4271dc
Jafet Henrique <jafetguerra6@gmail.com>
2020-08-27 21:41:28 -0300

    cc

--- models/registerModel.js
+++ models/registerModel.js
@@ -10,8 +10,8 @@ const addUser = (email, password, name, lastName) =>
   );
 
 const newUserIsValid = (email, password, confirmPassword, name, lastName) => {
-  const emailRegEx = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/;
-  const nameRegEx = /^[a-zA-Z]+$/i;
+  const emailRegEx = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}/;
+  const nameRegEx = /^[a-zA-Z]+/i;
   const messageArr = [];
   if (!emailRegEx.test(email)) messageArr.push('O email deve ter o formato email@mail.com');
 
