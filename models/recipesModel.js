389dd8010a76df406ce02bd9cd7bf2d35a6a9f50
Cardoso <lsc.cardosoo@gmail.com>
2020-08-28 16:38:42 -0300

    code climate

--- models/recipesModel.js
+++ models/recipesModel.js
@@ -1,29 +1,25 @@
 const connection = require('./connection');
 
-  const findAllRecipes = async () =>
-    connection()
-      .then((db) => db.getTable('recipes').select(['name', 'user', 'id']).execute())
-      .then((results) => results.fetchAll())
-      .then((recipes) => recipes.map(([ name, user, id]) => ({ name, user, id })));
+const findAllRecipes = async () =>
+  connection()
+    .then((db) => db
+      .getTable('recipes')
+      .select(['name', 'user', 'id'])
+      .execute())
+    .then((results) => results.fetchAll())
+    .then((recipes) => recipes.map(([name, user, id]) => ({ name, user, id })));
 
 const findRecipeById = async (id) =>
   connection()
     .then((db) => db
       .getTable('recipes')
       .select(['name', 'user', 'ingredients', 'instructions', 'id'])
-      .where('id = :id').bind('id', id).execute())
+      .where('id = :id')
+      .bind('id', id)
+      .execute())
     .then((results) => results.fetchOne())
-    .then(([name, user, ingredients, instructions, id]) => ({ name, user, ingredients, instructions, id }));
-// const findRecipeByName = async (input) =>
-//   connection()
-//     .then((db) => db.getTable('recipes').select().execute())
-//     .then((results) => results.objects)
-//     .then((recipes) => (
-//       input === ''
-//         ? recipes
-//         : recipes.filter(({ name }) => name.toLowerCase().includes(input.toLowerCase()))
-//     ));
-
+    .then(([name, user, ingredients, instructions, ...user]) =>
+      ({ name, user, ingredients, instructions, id: user.id }));
 
 const findRecipeByName = async (input) =>
   connection()
@@ -34,7 +30,7 @@ const findRecipeByName = async (input) =>
       .bind('input', `%${input}%`)
       .execute())
     .then((results) => results.fetchAll())
-    .then((recipes) =>  recipes.map(([name, user, id]) => ({ name, user, id })));
+    .then((recipes) => recipes.map(([name, user, id]) => ({ name, user, id })));
 
 module.exports = {
   findAllRecipes,
