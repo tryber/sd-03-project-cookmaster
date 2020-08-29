b92d7fb925fd63f099ea79a1124126988af32d6e
brenomesquita <brenomesquitaribeiro@gmail.com>
2020-08-29 05:07:54 -0300

    exercise 1 code-climete

--- controllers/cookController.js
+++ controllers/cookController.js
@@ -1,4 +1,5 @@
 const cookModel = require('../models/cookModel');
+const rescue = require('express-rescue');
 
 const listCook = async (_req, res) => {
   const recipes = await cookModel.getAll();
@@ -20,11 +21,10 @@ const searchRecipe = async (req, res) => {
 const lala = [];
 const newRecipe = (_req, res) => res.render('admin/newRecipe', { lala: [], name: '' });
 
-const setNewRecipe = async (req, res) => {
+const setNewRecipe = rescue(async (req, res) => {
   const { remove, name, ingredient, instructions, save } = req.body;
   if (remove !== undefined) {
-    lala.splice(remove, 1);
-    return res.render('admin/newRecipe', { lala, name });
+    return lala.splice(remove, 1).then(res.render('admin/newRecipe', { lala, name }))
   }
   if (save !== undefined && name.length > 0 && lala.length > 0 && instructions.length > 0) {
     await cookModel.setNewRecipes(req.body, req.user);
@@ -34,7 +34,7 @@ const setNewRecipe = async (req, res) => {
     lala.push(ingredient);
   }
   return res.render('admin/newRecipe', { lala, name });
-};
+});
 
 const cooks = async (req, res) => {
   const { id } = req.params;
