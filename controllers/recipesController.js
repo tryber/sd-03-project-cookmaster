a7642f56390f3df0429b6497bb3907e43164eeb3
Clayton Pereira <cpereiramt@gmail.com>
2020-08-29 18:55:50 -0400

    Ajustando cc

--- controllers/recipesController.js
+++ controllers/recipesController.js
@@ -2,7 +2,7 @@ const recipesModel = require('../models/recipesModel');
 
 const recipesList = async (req, res) => {
   const listData = await recipesModel.listRecipes();
-  res.render('home', { listData, usuario: req.user});
+  res.render('home', { listData, usuario: req.user });
 };
 
 module.exports = {
--- models/userModel.js
+++ models/userModel.js
@@ -1,13 +1,5 @@
-const  db  = require('./DbConnection');
+const db = require('./DbConnection');
 /* Quando você implementar a conexão com o banco, não deve mais precisar desse objeto */
-const TEMP_USER = {
-  id: 'd2a667c4-432d-4dd5-8ab1-b51e88ddb5fe',
-  email: 'taylor.doe@company.com',
-  password: 'password',
-  name: 'teste',
-  lastName: 'Doe',
-};
-
 /* Substitua o código das funções abaixo para que ela,
 de fato, realize a busca no banco de dados */
 
@@ -16,16 +8,16 @@ de fato, realize a busca no banco de dados */
  * @param {string} email Email do usuário a ser encontrado
  */
 const findByEmail = async (email) => {
-  const usuarios = await  db()
+  const usuarios = await db()
   .then((data) => data.getTable('users')
   .select()
   .where('email = :email')
   .bind('email', email)
-  .execute())
+  .execute());
 
 const user = usuarios.fetchAll();
-return user.map(([id,email, password, name, lastName]) => (
-  { id, email, password, name, lastName }
+  return user.map(([id,email2, password, name, lastName]) => (
+  { id, email2, password, name, lastName }
 ))[0];
 };
 
@@ -34,15 +26,15 @@ return user.map(([id,email, password, name, lastName]) => (
  * @param {string} id ID do usuário
  */
 const findById = async (id) => {
-  const usuarios = await  db()
+  const usuarios = await db()
   .then((data) => data.getTable('users')
   .select()
   .where('id = :id')
   .bind('id', id)
-  .execute())
+  .execute());
   const user = usuarios.fetchAll();
-return user.map(([id,email, password, name, lastName]) => (
-  { id, email, password, name, lastName }
+  return user.map(([id2, email, password, name, lastName]) => (
+  { id2, email, password, name, lastName }
 ))[0];
 };
 
