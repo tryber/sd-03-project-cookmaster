21bbf07eb069d41f1fef571ced5f2609e3a6a076
Clayton Guimaraes Leite Pereira <claytonpereira@mtprev.local>
2020-08-31 13:52:01 -0400

    Salvando alteração nos requisitos

--- models/DbConnection.js
+++ models/DbConnection.js
@@ -1,20 +1,22 @@
 const mysqlx = require('@mysql/xdevapi');
-
-const connection = () => (
-  mysqlx.getSession({
+let db_session;
+const connection = () => {
+  if (db_session) return Promise.resolve(db_session) 
+  return mysqlx.getSession({
     user: 'root',
     password: '',
     host: 'localhost',
     port: 33060,
     schema: 'cookmaster',
   })
-  .then((session) => (
-        session.getSchema('cookmaster')
-  ))
+  .then((session) => {
+      db_session = session.getSchema('cookmaster')
+      return db_session;
+  })
   .catch((err) => {
     console.error(err);
     // process.exit(1);
   })
-);
+};
 
 module.exports = connection;
