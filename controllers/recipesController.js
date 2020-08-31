0324f149a15f69e26ca67b3a17ac14a7fdde2b4c
Leticia Cristina Duarte Lima <leticiacristinaduartelima@Leticias-MacBook-Air.local>
2020-08-31 14:41:07 -0300

    Code Climate

--- controllers/recipesController.js
+++ controllers/recipesController.js
@@ -1,7 +1,7 @@
 const recipesModel = require('../models/recipesModel');
 const rescue = require('express-rescue');
 
-const renderRecipes = rescue(async(_req, res) => {
+const renderRecipes = rescue(async (_req, res) => {
   const recipes = await recipesModel.getAllRecipes();
 
   res.render('home', { recipes });
