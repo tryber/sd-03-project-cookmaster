39ab85f01928655fb47e18c0dd1d320f54cbc06f
Hfreitas <hfreitas86@gmail.com>
2020-08-31 19:36:57 -0300

    model ok

--- models/userModel.js
+++ models/userModel.js
@@ -90,8 +90,28 @@ const createUser = async (email, password, name, lastName) => {
   }
 };
 
+const updateUser = async (id, email, password, name, lastName) => {
+  try {
+    const db = await connection();
+    const updateQuery = await db
+      .getTable('users')
+      .update()
+      .set('email', email)
+      .set('password', password)
+      .set('name', name)
+      .set('lastName', lastName)
+      .where('id = :id')
+      .bind('id', id)
+      .execute();
+    return updateQuery;
+  } catch (error) {
+    return error;
+  }
+};
+
 module.exports = {
   findByEmail,
   findById,
   createUser,
+  updateUser,
 };
