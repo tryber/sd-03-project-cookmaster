21609e1b95e2f69fae67b2af6a382f075c411085
DiegoRafaelAF <draf0314@gmail.com>
2020-09-01 19:42:13 -0300

    alteraçao de rotas para test

--- index.js
+++ index.js
@@ -19,12 +19,11 @@ app.get('/admin', middlewares.auth(), (req, res) => {
   return res.render('admin/home', { user: req.user });
 });
 
-app.get('/login', controllers.userController.login);
-app.get('/login', controllers.userController.loginForm);
 app.get('/register', middlewares.auth(false), controllers.userController.registerForm);
-app.get('/logout', controllers.userController.logout);
-app.post('/login', controllers.userController.login);
 app.post('/register', middlewares.auth(false), controllers.userController.registerUser);
+
+app.get('/recipes/new', middlewares.auth(), controllers.recipesController.formRecipe);
+app.post('/recipes/new', middlewares.auth(), controllers.recipesController.newRecipes);
 
 app.get(
   '/recipes/search',
@@ -32,15 +31,19 @@ app.get(
   controllers.recipesController.listsharchRecipes,
 );
 
-app.post('/recipes/:id/delete', middlewares.auth(), controllers.recipesController.deleteRecipeUser);
-app.get('/recipes/new', middlewares.auth(), controllers.recipesController.formRecipe);
-app.post('/recipes/new', middlewares.auth(), controllers.recipesController.newRecipes);
-
 app.get('/recipes/:id', middlewares.auth(false), controllers.recipesController.listRecipesById);
 app.post('/recipes/:id', middlewares.auth(), controllers.recipesController.editRecipe);
+
 app.get('/recipes/:id/edit', middlewares.auth(), controllers.recipesController.formEditRecipe);
+
+app.post('/recipes/:id/delete', middlewares.auth(), controllers.recipesController.deleteRecipeUser);
 app.get('/recipes/:id/delete', middlewares.auth(), controllers.recipesController.deleteRecipeForm);
 
 app.get('/me/recipes', middlewares.auth(), controllers.recipesController.getUserRecipe);
+
+app.get('/login', controllers.userController.loginForm);
+
+app.get('/logout', controllers.userController.logout);
+app.post('/login', controllers.userController.login);
 
 app.listen(3000, () => console.log('Listening on 3000'));
