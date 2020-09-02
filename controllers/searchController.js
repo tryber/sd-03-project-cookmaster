71b39fe44a3aca37e7664dd9934b4edfae0d52cf
nato-x <alvares.renato@hotmail.com>
2020-09-01 22:41:41 -0300

    cc fix

--- controllers/searchController.js
+++ controllers/searchController.js
@@ -4,15 +4,10 @@ const { getAllRecipes } = require('../models/recipeModel');
 const searchRecipe = async (req, res) => {
   try {
     const { q } = req.query;
-
     // console.log('input', input)
     const recipes = q && (await findByName(q));
     // console.log('recipe', recipes);
-    if (!recipes || recipes === []) {
-      const all = await getAllRecipes();
-      // console.log('all', all)
-      if (q === '') return res.render('search', { recipes: null, user: req.user });
-    }
+    if (q === '') return res.render('search', { recipes: null, user: req.user });
     // console.log('----------------------------')
     if (req.user) return res.render('search', { recipes, user: req.user });
     return res.render('search', { recipes, user: null });
