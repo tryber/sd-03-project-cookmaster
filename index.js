7cf417a3c346316dbbc153fec3834b62ea5eb730
rsfdev <roger_faustino@hotmail.com>
2020-09-02 15:34:28 -0300

    Cookmaster Project

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 // Reading the cookie that contains the authentication token.
 const cookieParser = require('cookie-parser');
-// Starting 5
+// Starting 6
 const middlewares = require('./middlewares');
 const controllers = require('./controllers');
 
