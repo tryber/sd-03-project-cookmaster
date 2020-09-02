65d8ef1da57863015564a28f212cc0a821c720dc
Leticia Cristina Duarte Lima <leticiacristinaduartelima@Leticias-MacBook-Air.local>
2020-09-02 11:11:08 -0300

    Removing returns

--- controllers/userController.js
+++ controllers/userController.js
@@ -25,31 +25,31 @@ const register = rescue(async (req, res) => {
   const { email, password, passwordV, name, lastName } = req.body;
 
   if (!email || !email.match(/^([a-zA-Z0-9_\-.]+)@([a-zA-Z0-9_\-.]+)\.([a-zA-Z]{2,5})$/))
-    return res.render('admin/register', {
+    res.render('admin/register', {
       message: 'O email deve ter o formato email@mail.com',
       redirect: null,
     });
 
   if (!password || password.length < 6)
-    return res.render('admin/register', {
+    res.render('admin/register', {
       message: 'A senha deve ter pelo menos 6 caracteres',
       redirect: null,
     });
 
   if (!passwordV || String(password) !== String(passwordV))
-    return res.render('admin/register', {
+    res.render('admin/register', {
       message: 'As senhas tem que ser iguais',
       redirect: null,
     });
 
   if (!name || !name.match(/^[a-zA-Z]{3,}$/))
-    return res.render('admin/register', {
+    res.render('admin/register', {
       message: 'O primeiro nome deve ter, no mínimo, 3 caracteres, sendo eles apenas letras',
       redirect: null,
     });
 
   if (!lastName || !lastName.match(/^[a-zA-Z]{3,}$/))
-    return res.render('admin/register', {
+    res.render('admin/register', {
       message: 'O segundo nome deve ter, no mínimo, 3 caracteres, sendo eles apenas letras',
       redirect: null,
     });
