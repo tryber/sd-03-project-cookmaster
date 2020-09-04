deaf07c351e4c0cfcc04072f1a5ea7858bcbbc41
rsfdev <roger_faustino@hotmail.com>
2020-09-04 09:45:59 -0300

    Erro avaliador persiste

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 // Reading the cookie that contains the authentication token.
 const cookieParser = require('cookie-parser');
-// Starting 7
+// Starting 8
 const middlewares = require('./middlewares');
 const controllers = require('./controllers');
 
