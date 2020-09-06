223479914eaf858f6ff6fb187164c817a06ef8ad
DevT <aguiarthiag@gmail.com>
2020-09-05 22:35:43 -0300

    CC

--- controllers/registerController.js
+++ controllers/registerController.js
@@ -5,36 +5,57 @@ const registerForm = (_req, res) => (
     message: null,
   })
 );
-  
+
+const emailTest = /^(([^<>()[\].,;:\s@"]+(\.[^<>()[\].,;:\s@"]+)*)|(".+"))@(([^<>()[\].,;:\s@"]+\.)+[^<>()[\].,;:\s@"]{2,})$/i;
+const namesTest = /^[a-zA-Z]*$/;
+
+const verifyEmail = (email, arrMessage) => {
+  if (!email.match(emailTest)) {
+    return arrMessage = [ ...arrMessage , 'O email deve ter o formato email@mail.com'];
+  };
+  return arrMessage;
+};
+
+const verifyPassword = (password, arrMessage) => {
+  if (password.length < 6) {
+    return arrMessage = [ ...arrMessage , 'A senha deve ter pelo menos 6 caracteres'];
+  };
+  return arrMessage;
+};
+
+const verifyConfirm = (password, confirm, arrMessage) => {
+  if (password !== confirm) {
+    return arrMessage = [ ...arrMessage , 'As senhas tem que ser iguais']; 
+  };
+  return arrMessage;
+};
+
+const verifyName = (name, arrMessage) => {
+  if (name.length < 3 || !name.match(namesTest)) {
+   return arrMessage = [ ...arrMessage, 
+    'O primeiro nome deve ter, no mínimo, 3 caracteres, sendo eles apenas letras'];
+  }
+  return arrMessage;
+};
+
+const verifyLastName = (lastName, arrMessage) => {
+  if (lastName.length < 3 || !lastName.match(namesTest)) {
+    return arrMessage = [ ...arrMessage , 
+      'O segundo nome deve ter, no mínimo, 3 caracteres, sendo eles apenas letras"'];
+  }
+  return arrMessage;
+}
 const setRegister = async (req, res) => {
   const { email, password, confirm, name, lastName } = req.body;
-
-  const emailTest = /^(([^<>()[\].,;:\s@"]+(\.[^<>()[\].,;:\s@"]+)*)|(".+"))@(([^<>()[\].,;:\s@"]+\.)+[^<>()[\].,;:\s@"]{2,})$/i;
-  const namesTest = /^[a-zA-Z]*$/;
-
   let arrMessage = []
 
-  if (!email.match(emailTest)) {
-    arrMessage = [ ...arrMessage , 'O email deve ter o formato email@mail.com'];
-  };
+  arrMessage = verifyEmail(email, arrMessage);
+  arrMessage = verifyPassword(password, arrMessage);
+  arrMessage = verifyConfirm(password, confirm, arrMessage);
+  arrMessage = verifyName(name, arrMessage);
+  arrMessage = verifyLastName(lastName, arrMessage);
 
-  if (password.length < 6) {
-    arrMessage = [ ...arrMessage , 'A senha deve ter pelo menos 6 caracteres'];
-  };
-  
-  if (password !== confirm) {
-    arrMessage = [ ...arrMessage , 'As senhas tem que ser iguais']; 
-  };
-
-  if (name.length < 3 || !name.match(namesTest)) {
-    arrMessage = [ ...arrMessage , 'O primeiro nome deve ter, no mínimo, 3 caracteres, sendo eles apenas letras'];
-  }
-
-  if (lastName.length < 3 || !lastName.match(namesTest)) {
-    arrMessage = [ ...arrMessage , 'O segundo nome deve ter, no mínimo, 3 caracteres, sendo eles apenas letras"'];
-  }
-
-  if(arrMessage.length > 0) 
+  if(arrMessage.length > 0)
     return res.render('users/login', { message: arrMessage, redirect: null, });
   
   await register.addUser(email, password, name, lastName)
