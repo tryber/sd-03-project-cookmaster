9568169a26b9471e8042bbf4bf8aa11163e3cb12
potew <potew0@gmail.com>
2020-09-09 13:25:20 -0300

    Cacheando a conexão

--- models/connection.js
+++ models/connection.js
@@ -1,5 +1,8 @@
 const mysqlx = require('@mysql/xdevapi');
+const connect = require('../../B28-Store-Manager/model/connect');
 require('dotenv').config();
+
+let connection;
 
 const config = {
   user: process.env.MYSQL_USER,
@@ -10,10 +13,14 @@ const config = {
 };
 
 // Cria uma nova sessão automaticamente
-module.exports = async () =>
-  mysqlx.getSession(config)
-  .then((session) => session.getSchema('cookmaster'))
+module.exports = async () => {
+  if (connection) return Promise.resolve(connection);
+  return mysqlx.getSession(config)
+  .then((connection) => {
+    connection = session.getSchema('cookmaster')
+    return connection })
   .catch((err) => {
     console.error(err);
     process.exit(1);
   });
+}
