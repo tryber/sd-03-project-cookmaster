9edd7a800fe678b6ce2bec0eeda84fe02c8758ea
mirellaalves <mirella.alves@gmail.com>
2020-09-11 17:13:18 -0300

    code climate issues

--- models/userModel.js
+++ models/userModel.js
@@ -7,17 +7,20 @@ de fato, realize a busca no banco de dados */
  * Busca um usuário através do seu email e, se encontrado, retorna-o.
  * @param {string} email Email do usuário a ser encontrado
  */
-const findByEmail = async (email) => {
+const findByEmail = async (Email) => {
   return connection()
   .then((db) => db
     .getTable('users')
     .select(['id', 'email', 'password', 'first_name', 'last_name'])
     .where('email = :email')
-    .bind('email', email)
-    .execute()
+    .bind('email', Email)
+    .execute(),
   )
   .then((results) => results.fetchAll()[0])
-  .then(([id, email, password, first_name, last_name]) => ({ id, email, password, first_name, last_name }))
+  .then(
+    ([id, email, password, firstName, lastName]) =>
+    ({ id, email, password, firstName, lastName })
+  )
   .catch((err) => console.error(err));
 };
 
@@ -25,17 +28,20 @@ const findByEmail = async (email) => {
  * Busca um usuário através do seu ID
  * @param {string} id ID do usuário
  */
-const findById = async (id) => {
+const findById = async (Id) => {
   return connection()
   .then((db) => db
     .getTable('users')
     .select(['id', 'email', 'password', 'first_name', 'last_name'])
     .where('id = :id')
-    .bind('id', id)
-    .execute()
+    .bind('id', Id)
+    .execute(),
   )
   .then((results) => results.fetchAll()[0])
-  .then(([id, email, password, first_name, last_name]) => ({ id, email, password, first_name, last_name }))
+  .then(
+    ([id, email, password, firstName, lastName]) =>
+    ({ id, email, password, firstName, lastName })
+  )
   .catch((err) => console.error(err));
 };
 
