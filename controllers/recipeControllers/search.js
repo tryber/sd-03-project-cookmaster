e6571f2059fd171986150860beefc9324e8e0837
wenderson <contato@wendersonbelko.com>
2020-09-17 18:51:39 -0300

    putz, esqueci uma letra ;-;

--- controllers/recipeControllers/search.js
+++ controllers/recipeControllers/search.js
@@ -3,9 +3,7 @@ const { findRecipesByQuery } = require('../../models');
 const search = async (req, res) => {
   const { buscar } = req.query;
 
-  if (buscar === '') {
-    return res.render('search', { recipes: null, user: req.user });
-  }
+  if (buscar === '') return res.render('search', { recipes: null, user: req.user });
 
   const recipes = await findRecipesByQuery(buscar);
 
--- models/recipeModels/findRecipeByQuery.js
+++ models/recipeModels/findRecipeByQuery.js
@@ -1,20 +1,22 @@
 const { connect } = require('../connect');
 
-const findRecipesByQuery = async (input) =>
+const findRecipesByQuery = async (search) =>
   connect()
     .then((db) =>
       db
         .getTable('recipes')
-        .select(['name', 'user', 'id'])
-        .where('name like :input')
-        .bind('input', `%${input}%`)
+        .select(['id', 'user', 'name'])
+        .where('name like :name')
+        .bind('name', `%${search}%`)
         .execute(),
     )
-    .then(async (results) => results.fetchAll())
-    .then(async (recipes) => recipes.map(([name, user, id]) => ({ name, user, id })))
-    .catch((err) => {
-      console.error(err);
-      process.exit(1);
-    });
+    .then((results) => results.fetchAll())
+    .then((recipes) =>
+      recipes.map(([id, user, name]) => ({
+        id,
+        user,
+        name,
+      })),
+    );
 
 module.exports = findRecipesByQuery;
